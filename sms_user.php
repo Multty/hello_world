0fbfd1b84fc8a2013f52fb96e990413c92b88aa0
Multty <lanrewajuakanji@gmail.com>
2017-01-11 01:11:01 -0100

    Update sms_user.php

--- sms_user.php
+++ sms_user.php
@@ -1,1 +1,31 @@
+<?php
 
+function sendsms_post ($url, array $params) {
+$params = http_build_query($params);
+$ch = curl_init(); 
+curl_setopt($ch, CURLOPT_URL,$url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+curl_setopt($ch, CU
+RLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $params);   
+$output=curl_exec($ch);
+curl_close($ch);
+return $output;        
+}
+$message = "Your verification code is: 3456778";
+$senderid = "CashClub";
+$recipients = $pfone;
+$sms_array = array (
+'username' => $newmeq,
+'password' => $ictclub,
+'message'   => $message,
+'sender'    => $senderid,
+'recipient' => $recipients
+);
+
+$url = "https://api.smartsmssolutions.com/smsapi.php";
+$post_sms = sendsms_post($url, $sms_array);
+echo $post_sms;
+
+
+?>
